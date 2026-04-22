21f5cb1d11ea40b5c70141cebb41bc61c5594660
Suhendar Aryadi <suhendararyadi@gmail.com>
2026-04-22 13:46:20 +0700

    feat(students): add create/store/edit/update routes

--- routes/students.php
+++ routes/students.php
@@ -11,11 +11,15 @@ Route::middleware(['auth', 'verified', EnsureTeamMembership::class.':admin'])
     ->group(function () {
         Route::get('students', [StudentController::class, 'index'])->name('index');
 
-        // Static import routes BEFORE {user} wildcard
+        // Static routes BEFORE {user} wildcard
+        Route::get('students/create', [StudentController::class, 'create'])->name('create');
+        Route::post('students', [StudentController::class, 'store'])->name('store');
         Route::get('students/import', [StudentImportController::class, 'create'])->name('import');
         Route::post('students/import', [StudentImportController::class, 'store'])->name('import.store');
         Route::get('students/import/template', [StudentImportController::class, 'template'])->name('import.template');
 
         // Wildcard after static routes
+        Route::get('students/{user}/edit', [StudentController::class, 'edit'])->name('edit');
+        Route::patch('students/{user}', [StudentController::class, 'update'])->name('update');
         Route::delete('students/{user}', [StudentController::class, 'destroy'])->name('destroy');
     });
